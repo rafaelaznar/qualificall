04b2c042290fe9868b3c8d93efb7ac1a3cddf92f
RAFAEL AZNAR <rafaaznar@gmail.com>
2021-10-13 09:44:30 +0200

    cambios en clase

--- src/main/java/net/ausiasmarch/sepivos/CORSfilter.java
+++ src/main/java/net/ausiasmarch/sepivos/CORSfilter.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package net.ausiasmarch.sepivos;
 
 import java.io.IOException;
@@ -74,7 +70,7 @@ public class CORSfilter implements Filter {
 
     @Override
     public void destroy() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       
     }
 
 }
--- src/main/java/net/ausiasmarch/sepivos/session.java
+++ src/main/java/net/ausiasmarch/sepivos/session.java
@@ -86,7 +86,7 @@ public class session extends HttpServlet {
 //                            name = null;
 //                        }
                         if (name != null) {
-                            if (name == "admin") {
+                            if (name.equalsIgnoreCase("admin")) {
                                 response.setStatus(HttpServletResponse.SC_OK);
                                 out.print(oGson.toJson("QWERTY"));
                             } else {
