9b113145e74376350ed720cdcfc0d5685c2f109d
raznara <raznara@s09-prof>
2022-02-04 12:38:56 +0100

    database

--- database_old.sql
+++ database_old.sql
@@ -4,7 +4,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:3306
--- Tiempo de generación: 04-02-2022 a las 11:36:51
+-- Tiempo de generación: 03-02-2022 a las 12:43:37
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 8.1.2
 
@@ -25,6 +25,38 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `code`
+--
+
+CREATE TABLE `code` (
+  `id` bigint(20) NOT NULL,
+  `description` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `code`
+--
+
+INSERT INTO `code` (`id`, `description`) VALUES
+(1, '2021-2022');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `grade`
+--
+
+CREATE TABLE `grade` (
+  `id` bigint(20) NOT NULL,
+  `user_id_from` bigint(20) NOT NULL,
+  `user_id_to` bigint(20) NOT NULL,
+  `grade` decimal(10,0) NOT NULL,
+  `time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `user`
 --
 
@@ -34,14 +66,25 @@ CREATE TABLE `user` (
   `surname` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `login` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `active` tinyint(1) NOT NULL,
-  `user_type_id` bigint(20) NOT NULL,
-  `professor_id` bigint(20) NOT NULL
+  `code_id` bigint(100) NOT NULL,
+  `active` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `code`
+--
+ALTER TABLE `code`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `grade`
+--
+ALTER TABLE `grade`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `user`
@@ -52,6 +95,18 @@ ALTER TABLE `user`
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `code`
+--
+ALTER TABLE `code`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `grade`
+--
+ALTER TABLE `grade`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `user`
